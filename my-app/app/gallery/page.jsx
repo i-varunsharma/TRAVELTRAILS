e00309eaede7e269679e7fae947f3db0b1647656
Varun Sharma <galvevarun@gmail.com>
2025-05-13 22:21:11 +0530

    Update page.jsx

--- my-app/app/gallery/page.jsx
+++ my-app/app/gallery/page.jsx
@@ -3,9 +3,18 @@ import Navbar from '../about/components/AddressBar';
 
 const Gallery = () => {
   return (
-    <>
-    <Navbar/>
-    </>
+  <div className="relative min-h-screen">
+      {/* Background image - moved to separate element */}
+      <div className="fixed inset-0 bg-[url('/bgimg.jpg')] bg-cover bg-center"></div>
+      
+      {/* Your existing overlay with reduced opacity */}
+      <div className="fixed inset-0 bg-black/30 backdrop-blur-sm"></div>
+      
+      {/* Content container - added relative positioning */}
+      <div className="relative pt-20">
+        <Navbar/>
+      </div>
+    </div>
   )
 }
 
